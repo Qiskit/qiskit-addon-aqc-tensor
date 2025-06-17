8adba2171cd439608ed0a51750695690ced1dc5a
Kevin J. Sung <kevjsung@umich.edu>
2025-06-17 12:55:55 -0400

    add comment

--- qiskit_addon_aqc_tensor/ansatz_generation/parametrize_circuit.py
+++ qiskit_addon_aqc_tensor/ansatz_generation/parametrize_circuit.py
@@ -127,6 +127,7 @@ def parametrize_circuit(
 
     for inst in qc.data:
         operation = inst.operation
+        # Ignore GlobalPhaseGate to avoid introducing a degenerate optimization parameter
         if operation.name == "global_phase":
             continue
         original_params = operation.params
